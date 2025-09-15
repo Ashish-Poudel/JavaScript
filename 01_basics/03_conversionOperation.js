775593f2dcd62b33ec0a1a366ad795d0995eb5ba
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-15 13:07:16 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score1 = 45
+let score2= "45"
+// console.log(typeof score1);
+// console.log(typeof(score2));
+
+let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber);
+
+// if the score is null, type is 0
+// if the score is undefined, type is NaN(Not a Number)
+// if the score is true, o/p is 1 and if false 0
+// if string , again NaN
+
+let isLoggedIn = "Aashish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=>true
+// ""=>false
+// "Abc"=>true
+
+let someNumber=45
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
