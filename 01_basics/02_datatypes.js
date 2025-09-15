ba395c87152f09ccad715586230f8d978fb0a5b9
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-15 13:08:01 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+
+// alert(5+2) // we are using nodejs, not browser
+
+console.log(4+3)
+console.log("Aashish")
+
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
+//functiom
+//array
+//date
+
+console.log(typeof "Aashish")
+console.log(typeof null)// typeof null is object, it is a bug in JS
