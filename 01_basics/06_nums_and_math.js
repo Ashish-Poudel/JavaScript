c2d6c4f1fff10e163299cbe3d3595c07b984c411
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-16 10:33:35 +0000

    Numbers and Maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const bal = 500
+const amount = new Number(200)
+// console.log(amount)
+
+// console.log(amount.toString().length)
+// console.log(amount.toFixed(2))
+
+const otherNumber = 45.679456
+// console.log(otherNumber.toPrecision(3))
+
+const num = 30000
+// console.log(num.toLocaleString('en-IN'))
+
+
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(9.7))
+// console.log(Math.ceil(3.4))
+// console.log(Math.floor(2.3))
+// console.log(Math.min(4,6,2,8))
+// console.log(Math.max(4,6,2,8))
+
+console.log(Math.random()) //betn 0 and 1
+console.log((Math.random()*10)+1)  
+
+const min=10
+const max=30
+
+console.log(Math.floor(Math.random()*(max-min+1) )+min)
