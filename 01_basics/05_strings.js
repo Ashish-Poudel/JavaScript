a47c0f87a19508b206fdf482372065c3c9b8aed7
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-16 10:33:00 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name="Aashish"
+const age=20
+// console.log(name+" Age:"+age)
+
+// console.log(`Hello  my name is ${name} and age is ${age}`)
+
+const discordName=new String('Poudel')
+// console.log(discordName.__proto__)
+
+// console.log(discordName.length) 
+// console.log(discordName.toUpperCase())
+// console.log(discordName.charAt('2'))
+// console.log(discordName.indexOf('e'))
+
+const newString = discordName.substring(0,4)
+console.log(newString)
+
+const anotherString = discordName.slice(-5,5)
+console.log(anotherString)
+
