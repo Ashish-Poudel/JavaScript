948d8eb3eb3d4a39f0c43fcfe953ee1b83e849cf
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-18 10:23:17 +0000

    Functions in JS

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,63 @@
+
+function sayMyName(){
+    console.log("A");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(4, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "Ram"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Aashish"))
+// console.log(loginUserMessage("Aashish"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(300, 400, 500, 2000))
+
+const user = {
+    username: "Aashish",
+    prices: 399
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Ram",
+    price: 299
+})
+
