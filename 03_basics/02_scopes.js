305d250f97bb2dbe8da2d08e09610e28031b88dc
Ashish-Poudel <pdlaashish2005@gmail.com>
2025-09-18 11:18:14 +0000

    Scopes in JS

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,52 @@
+// var c = 400
+let a = 600 
+
+if(true){
+  let a = 20
+  const b = 50
+  
+} 
+
+// for (let i = 0; index < array.length; index++) {
+//     const element = array[i];   
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one(){
+    const username = "Aashish"
+     
+    function two(){
+        const website = "google"
+        // console.log(username)
+        // console.log(website)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+if(true){
+    const username = "Aashish"
+    if(username=="Aashish"){
+        const website = " google"
+        // console.log(username+website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+function addone(num){
+   return num + 1
+}
+addone(5) // we can also write this before function
+ 
+
+const addTwo = function (num){
+    return num + 2
+}
+addTwo(5) // we can not write before function
